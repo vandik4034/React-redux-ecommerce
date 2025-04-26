4a0a69292689fd32bd36f0873b05a21e1b2a2eee
hardik rathod <hardikrathod8153@gmail.com>
2025-04-26 15:45:46 +0530

    add new data

--- Server/db.js
+++ Server/db.js
@@ -1,32 +1,32 @@
-import sql from "mssql";
+// import sql from "mssql";
 
-export const config = {
-  server: "DESKTOP-EQ55Q8H\\SPART",
-  database: "ecommerce",
-  options: {
-    trustServerCertification: true,
-    encrypt: false,
-  },
+// export const config = {
+//   server: "DESKTOP-EQ55Q8H\\SPART",
+//   database: "ecommerce",
+//   options: {
+//     trustServerCertification: true,
+//     encrypt: false,
+//   },
 
-  authentication: {
-    type: "default",
-    options: {
-      userName: "sa",
-      password: "Hardik@8153",
-      domain: "yourDomainname",
-    },
-  },
-};
+//   authentication: {
+//     type: "default",
+//     options: {
+//       userName: "sa",
+//       password: "Hardik@8153",
+//       domain: "yourDomainname",
+//     },
+//   },
+// };
 
-const connectToDB = async () => {
-  try {
-    const pool = await sql.connect(config);
-    console.log(" SQL Server connect sucessful");
-    return pool;
-  } catch (err) {
-    console.error(" Error:", err);
-    throw err;
-  }
-};
+// const connectToDB = async () => {
+//   try {
+//     const pool = await sql.connect(config);
+//     console.log(" SQL Server connect sucessful");
+//     return pool;
+//   } catch (err) {
+//     console.error(" Error:", err);
+//     throw err;
+//   }
+// };
 
-export { connectToDB, sql };
+// export { connectToDB, sql };
